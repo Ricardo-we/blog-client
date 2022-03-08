f7816f43585f2bacad01f5e74799c7742a48ff99
Ricardo-we <totgamer75@gmail.com>
2022-03-07 21:29:51 -0600

    blog navbar updated

--- src/views/blog-views/BlogNavBar.jsx
+++ src/views/blog-views/BlogNavBar.jsx
@@ -9,6 +9,10 @@ function BlogNavBar() {
                 <Nav className="me-auto">
                     <Link to="/" className="nav-link">Home</Link>
                 </Nav>
+                <div className="me-auto">
+                    <Link to="/login" className="nav-link me-auto">Login</Link>
+                    <Link to="/create-user" className="nav-link btn btn-outline-primary me-auto">Create user</Link>
+                </div>
             </Container>
         </Navbar>
     );
